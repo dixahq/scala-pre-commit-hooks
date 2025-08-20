31fc2d33347c074a4d05eb498aea1d0582580f2a
Christian Rasmussen <cwe@dixa.com>
2025-08-20 10:36:15 +0200

    Use scala-cli if scalafmt not present
    scala-cli can download the correct scala native binary of scalafmt on
    demand, which has an even faster startup time. Just think of all the
    seconds we'll save!

--- shell/scalafmt-autofix.sh
+++ shell/scalafmt-autofix.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-FILES_TO_FORMAT=$(scalafmt --list $@)
+CMD="scalafmt"
+BETTER_ARGS="$@"
+LIST_ARG="--list"
+if ! command -v scalafmt > /dev/null 2>&1
+then
+	BETTER_ARGS=""
+	for word in $@
+	do
+		BETTER_ARGS="${BETTER_ARGS} -F '$word'"
+	done
+	CMD="scala-cli format"
+	LIST_ARG="-F '--list'"
+fi
 
+echo "Checking formatting..."
+LIST_CMD="$CMD $LIST_ARG $BETTER_ARGS"
+echo "$LIST_CMD"
+FILES_TO_FORMAT=$(eval "$LIST_CMD")
+
+FORMAT_CMD="$CMD $BETTER_ARGS"
 echo "Formatting..."
-echo "scalafmt $@"
-scalafmt $@
+echo "$FORMAT_CMD"
+eval "$FORMAT_CMD"
+
 
 echo "Staging files:"
 echo $FILES_TO_FORMAT
-git add $FILES_TO_FORMAT
+if [ -n "${FILES_TO_FORMAT}" ]; then
+	git add $FILES_TO_FORMAT
+fi
