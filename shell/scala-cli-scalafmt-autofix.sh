17e0918984694dab8f35ebbeff285b469fe78159
Christian Rasmussen <cwe@dixa.com>
2025-08-20 16:27:58 +0200

    pr feedback

--- shell/scala-cli-scalafmt-autofix.sh
+++ shell/scala-cli-scalafmt-autofix.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 CMD="scala-cli format"
-LIST_ARG="-F '--list'"
 BETTER_ARGS=""
 for word in $@
 do
 	BETTER_ARGS="${BETTER_ARGS} -F '$word'"
 done
 
-echo "Checking formatting..."
-LIST_CMD="$CMD $LIST_ARG $BETTER_ARGS"
-echo "$LIST_CMD"
-FILES_TO_FORMAT=$(eval "$LIST_CMD")
-
 FORMAT_CMD="$CMD $BETTER_ARGS"
 echo "Formatting..."
 echo "$FORMAT_CMD"
 eval "$FORMAT_CMD"
 
-
+FILES_TO_STAGE=$(git diff --cached --name-only)
 echo "Staging files:"
-echo $FILES_TO_FORMAT
-if [ -n "${FILES_TO_FORMAT}" ]; then
-	git add $FILES_TO_FORMAT
+echo $FILES_TO_STAGE
+if [ -n "${FILES_TO_STAGE}" ]; then
+	git add $FILES_TO_STAGE
 fi
